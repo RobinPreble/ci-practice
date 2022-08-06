a41429189cd5da22bcf0d9844c849d350419e515
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-08-06 16:23:21 -0700

    added bare bones code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
+}
+
+fn return_word(word: &str) -> &str {
+    word
+}
+fn return_upper(word: &str) -> String {
+        word.to_uppercase()
+    }
+
+#[cfg(test)]
+mod test {
+    use crate::{return_word, return_upper};
+
+    #[test]
+    fn tests () {
+        assert_eq!(return_word("greg"), "greg")
+    }
+
 }
